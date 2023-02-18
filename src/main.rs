5bd52b0759e62ba997608c690bed15de6ad88b7a
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-18 15:52:14 +0200

    Parse the command-line arguments using structopt.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    let input_path = args.input;
+
+    println!("{:?}", input_path)
 }
