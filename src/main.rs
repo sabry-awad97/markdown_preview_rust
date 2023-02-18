532e707024b3a1f56724e4495967d46542152b38
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-18 15:56:46 +0200

    Read the contents of the input file and parse it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::path::PathBuf;
+use pulldown_cmark::{html, Options, Parser};
+use std::{fs, path::PathBuf};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -11,5 +12,11 @@ fn main() {
     let args = Cli::from_args();
     let input_path = args.input;
 
-    println!("{:?}", input_path)
+    let input = fs::read_to_string(input_path).expect("Failed to read input file");
+    let parser = Parser::new_ext(&input, Options::all());
+
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    println!("{}", html_output)
 }
