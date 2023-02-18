3d612aa85e9213646d1d2db839eebb6f53a3aa2e
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-18 16:32:37 +0200

    optimized version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use pulldown_cmark::{html, Options, Parser};
-use std::io::Write;
-use std::process::Command;
-use std::{fs, path::PathBuf};
+use std::{
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+    process::{Command, Stdio},
+};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -10,29 +13,28 @@ struct Cli {
     input: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
-    let input_path = args.input;
+    let input_path = &args.input;
 
-    let input = fs::read_to_string(input_path).expect("Failed to read input file");
+    let input = fs::read_to_string(input_path)?;
     let parser = Parser::new_ext(&input, Options::all());
 
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
-
 
     let template = format!(
         r#"<html><head><title>"Markdown Preview"</title></head><body>{body}</body></html>"#,
         body = &html_output
     );
 
-    let mut file = fs::File::create("preview.html").expect("Failed to create temporary file");
-    write!(file, "{}", template).expect("Failed to write to temporary file");
+    let mut file = File::create("preview.html")?;
+    write!(file, "{}", template)?;
 
     Command::new("cmd")
-        .arg("/c")
-        .arg("start")
-        .arg("preview.html")
-        .spawn()
-        .expect("Failed to open default browser");
+        .args(&["/C", "start", "preview.html"])
+        .stdout(Stdio::null())
+        .spawn()?;
+
+    Ok(())
 }
